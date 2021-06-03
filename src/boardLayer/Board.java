446c9e97ce8e511b739240546c0bbd19893f09e2
China <guilherme.hiraoka@gmail.com>
2021-06-02 21:54:50 -0300

    Criando implementando class Board e Piece

--- src/boardLayer/Board.java
+++ src/boardLayer/Board.java
@@ -2,4 +2,39 @@ package boardLayer;
 
 public class Board {
 
+	private int rows;
+	private int cols;
+	private Piece[][] pieces; //pq eu to criando um objeto matriz de peças?!
+		
+	public Board(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		pieces = new Piece[rows][cols];
+	}
+	
+	 
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getCols() {
+		return cols;
+	}
+
+	public void setCols(int cols) {
+		this.cols = cols;
+	}
+
+	/*
+	 public Piece removePiece(Position position) {
+		return piece;
+	}
+	
+	private boolean positionExists(Position position) {
+		return (position.getRow() != 0) and (position.getColumn() != 0));  
+	}*/
 }
--- src/boardLayer/piece.java
+++ src/boardLayer/piece.java
@@ -1,6 +1,18 @@
 package boardLayer;
 
-public class piece extends Position {
+public class Piece extends Position {
+	
+	protected Position position;
+	private Board board; // seguindo diagrama de classes a relação entre classe Board e Piece implica na criação de objeto board em Piece e piece em Board; 
+	
+	public Piece(Board board) { // relação do diagrama, Piece recebendo objeto board
+		this.board = board;
+		position = null;
+	}
+	
+	protected Board getBoard() {
+		return board;
+	}
 	
 	public boolean[][] possibleMoves() {
 		return possibleMoves();
