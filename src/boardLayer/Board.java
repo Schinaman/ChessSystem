84d682441ef068b70da888f1efea43eb25a320a7
China <guilherme.hiraoka@gmail.com>
2021-06-14 20:32:45 -0300

    BoardException class e Programação defensiva

--- src/boardLayer/Board.java
+++ src/boardLayer/Board.java
@@ -4,51 +4,68 @@ public class Board {
 
 	private int rows;
 	private int cols;
-	private Piece[][] pieces; //pq eu to criando um objeto matriz de peças?! É uma propriedade, cada elemento da matriz é um objeto que representa qual peça esta alocada em determinada posição no tabuleiro; 
-		
+	private Piece[][] pieces; // pq eu to criando um objeto matriz de peças?! É uma propriedade, cada elemento
+								// da matriz é um objeto que representa qual peça esta alocada em determinada
+								// posição no tabuleiro; A Matriz representa cada unidade de espaço do
+								// tabuleiro, espaço em branco é uma piece = null;
+
 	public Board(int rows, int cols) {
+		
+		if (rows<=0 || cols<=0) {// Programação defensiva
+			throw new BoardException("Erro ao Criar tabuleiro; necessita ao menos 1 linha ou 1 coluna");
+		}
+		
 		this.rows = rows;
 		this.cols = cols;
-		pieces = new Piece[rows][cols]; // pra que que serve isso?; E aonde tá isso aqui no diagrama? será que é no (1 pra -> *)
+		pieces = new Piece[rows][cols]; // pra que que serve isso?; E aonde tá isso aqui no diagrama? será que é no (1
+										// pra -> *)
 	}
-	
-	 
+
 	public int getRows() {
 		return rows;
 	}
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getCols() {
 		return cols;
 	}
 
-	public void setCols(int cols) {
-		this.cols = cols;
-	}
 
-	
-	
-	public Piece piece(int row,int col) {
+	public Piece piece(int row, int col) {
+		if (!positionExists(row,col)) { // Programacao defensiva
+			throw new BoardException("Position não existe no tabuleiro");
+		}
 		return pieces[row][col];
 	}
-	
+
 	public Piece piece(Position position) {
+		if (!positionExists(position)) { // Programacao defensiva
+			throw new BoardException("Position não existe no tabuleiro: " + position);
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
-		pieces[position.getRow()][position.getColumn()] = piece; 
-		piece.position = position; //position como propriedade da piece
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Ja existe uma peca nesta posicao: " + position);
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position; // position como propriedade da piece
 	}
 	/*
-	 public Piece removePiece(Position position) {
-		return piece;
+	 * public Piece removePiece(Position position) { return piece; }
+	 */
+
+	private boolean positionExists(int row, int col) {
+		return (row >= 0 && row <= rows && col >= 0 && col <= cols);
 	}
-	
 	private boolean positionExists(Position position) {
-		return (position.getRow() != 0) and (position.getColumn() != 0));  
-	}*/
+		return (positionExists(position.getRow(),position.getColumn()));
+	}
+
+	private boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) { // Programacao defensiva
+			throw new BoardException("Position não existe no tabuleiro: " + position );
+		}
+		return (piece(position)!= null);
+	}
+
 }
--- src/boardLayer/piece.java
+++ src/boardLayer/piece.java
@@ -5,8 +5,8 @@ public class Piece extends Position { // aonde tá essa herença no diagrama?
 	protected Position position;
 	private Board board; // seguindo diagrama de classes a relação entre classe Board e Piece implica na criação de objeto board em Piece e piece em Board; 
 	
-	public Piece(Board board) { // relação do diagrama, Piece recebendo objeto board
-		this.board = board;
+	public Piece(Board board) { // relação do diagrama, Piece recebendo objeto board;
+		this.board = board; // eu tenho uma tabuleiro para cada peça? é para definir qual tabuleiro a peça esta associada
 		position = null; // quer dizer que a peça não foi colocada no tabuleiro ainda; poderia ser omitido;
 	}
 	
