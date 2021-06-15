dee95a88b307078f4114ad9b7fcb36ba551d82d0
China <guilherme.hiraoka@gmail.com>
2021-06-14 21:48:46 -0300

    ChessException e ChessPosition(b,9) classes

---
+++ src/chessLayer/ChessPosition.java
@@ -0,0 +1,54 @@
+package chessLayer;
+
+import boardLayer.BoardException;
+import boardLayer.Position;
+
+public class ChessPosition {
+
+	private char column;
+	private int row;
+
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("A ChessPosition não existe: " + column + "," + row);
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public void setColumn(char column) {
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	protected Position toPosition() {
+		//if (column < 'a' || column > 'h' || row < 1 || row > 8) { // NAO FAZ SENTIDO ESSA PROGRAMACAO DEFENSIVA aqui?
+		//	throw new ChessException("A ChessPosition não existe: " + column + "," + row);
+		//}
+		return new Position(8-row, column - 'a');
+	}
+
+	protected static ChessPosition fromPosition(Position position) {// pq é static? pq eu vou usar esse metodo em todas as classes referenciando esse formato de posicao?
+		//if (!positionExists(position)) { // NAO FAZ SENTIDO ESSA PROGRAMACAO DEFENSIVA aqui?
+		//	throw new BoardException("Position não existe no tabuleiro: " + position );
+		//}				
+		return new ChessPosition((char)('a' - position.getColumn()), 8- position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return ""+ column + row;
+	}
+	
+}
